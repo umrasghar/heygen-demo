a66b7b69245cad324c8cbc375a1f89c3ac6cd0de
Umar Asghar <umicool098@gmail.com>
2025-09-25 06:21:02 +0500

    Connect Avatar and Enter LiveKit Room

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,82 @@
-import Image from "next/image";
+'use client'
+
+import { useState } from 'react';
+import { useAvatarSession } from './hooks/useAvatarSession'
+import { stat } from 'fs';
+
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const {
+    status,
+    sessionData,
+    isConnected,
+    vidoRef,
+    createSession,
+    disconnectSession
+  } = useAvatarSession()
+
+  return (
+    <main className="min-h-screen bg-gray-50 p-8">
+      <div className="max-w-3xl mx-auto text-center">
+        <h1 className="text-4xl font-bold">HeyGen LiveKit Demo</h1>
+        <p className="mt-4 text-lg">Welcome to the HeyGen LiveKit Demo application!</p>
+      </div>
+   
+      <div className="bg-white rounded-lg shadow-md p-6 m-4">
+          <p>Status: {status}</p>
+      </div>
+
+      {sessionData && (
+        <div className="bg-blue-50 rounded p-6 m-4">
+          <p className='text-blue-800 font-medium'>Session ID: {sessionData.session_id}</p>
+          <p className='text-blue-600 font-sm'>WebSocket URL: {sessionData.url}</p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+      )}
+
+      <div className='relative aspect-video bg-gray-100 rounded-lg overflow-hidden'>
+        <video 
+          ref= {vidoRef}
+          autoPlay
+          playsInline
+          className={`object-contain w-full h-full ${isConnected ? 'block' : 'hidden'}`}
+        />
+
+        {!isConnected && (
+          <div className='absolute inset-0 flex items-center justify-center'>
+            <p className='text-gray-500 text-lg'>
+              {status.includes('Creating') || status.includes('Connecting')
+                ? 'Loading...'
+                : 'Avatar will appear here.'
+              }
+            </p>
+          </div>
+        )}
+
+      </div>
+
+
+
+      <div className='mt-4'>
+        <button
+          className="bg-blue-500 text-white px-4 py-2 rounded mr-2 hover:bg-blue-700"
+          onClick={createSession}
+          disabled={status.includes('Creating') || status.includes('Connecting') || isConnected}
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          Start Session
+        </button>
+
+        <button
+          className="bg-red-500 text-white px-4 py-2 rounded mr-2 hover:bg-blue-700"
+          onClick={disconnectSession}
+          disabled={!isConnected}
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+          End Session
+        </button>
+
+
+      </div>
+    
+    </main>
+  )
 }
